bfec71ce661032edf03850b492d9f749a9896d18
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2021-08-29 14:19:46 -0700

    Implement initial calculation
    Calculates the average season long expected points for each position and
    compares each player to the average.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Experimental: Fantasy Football Metrics 2021
+
+Uses the R language to calculate fantasy football metrics.
+
+The initial scratchpad uses data from [NFL Fantasy Football](https://fantasy.nfl.com). Using the NFL's projected points for each player, it calculates an average for all positions and scores each player based on their projected value over the average value for the position.
+
+For example, of the top 100 players, all running backs are projected to average `220.7` points each. If Christian McCaffrey is projected to score `312.63` points for the season, his points over expected is `91` because `312.63 - 220.7 = 91` (rounded).
--- scratch.R
+++ scratch.R
@@ -3,16 +3,21 @@ library(rvest)
 
 html <- read_html("data/nfl-player-rankings.html")
 name <- html_elements(html, "li a.playerName") %>% html_text2()
-projected_season_score <-
+projected_points <-
   html_elements(html, "li span.projected") %>% html_text2() %>% readr::parse_double()
 position_and_team <-
   html_elements(html, "li a.playerName + em") %>% html_text2()
 
 players <-
-  data.frame(name, projected_season_score, position_and_team) %>%
-  separate(position_and_team, c("position", "team"), " - ")
+  data.frame(name, projected_points, position_and_team) %>%
+  separate(position_and_team, c("position", "team"), " - ") %>%
+  group_by(position) %>%
+  mutate(mean_replacement_points = mean(projected_points)) %>%
+  ungroup() %>%
+  mutate(value_over_replacement = as.integer(projected_season_score - mean_replacement_points)) %>%
+  arrange(desc(value_over_replacement))
 
-
+write.csv(players, file="output/replacement_points_rank.csv")
 
 ## ---
 
